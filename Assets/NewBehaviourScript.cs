1a2a717dd36d21c54eda2e1a568cc8a936b7801d
rStar <starlove1247@gmail.com>
2025-03-12 10:22:00 +0800

    偵測到對方時，將自己與對方隱藏

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,15 +1,23 @@
+using System;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private void Start()
+    {
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log($"OnTriggerEnter2D");
+        // gameObject.SetActive(false);
+        Debug.Log("OnTriggerEnter2D");
+        gameObject.SetActive(false);
+        col.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
         Debug.Log($"OnCollisionEnter2D");
+        col.gameObject.SetActive(false);
     }
-
 }
